2a40f0f816670215e099644d1811ebcc26adc561
Angel Molina <64046993+Angel-Molina20@users.noreply.github.com>
2021-09-05 15:20:48 -0500

    4to commit
    Se realizo el controlador del producto (CRUD)

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,19 +1,41 @@
-export const CrearProducto = (req, res) =>{
+import Product from '../models/Product'
+
+export const CrearProducto = async (req, res) =>{
+
+    const {name, category, price, imgUrl} = req.body
+    const nuevoProducto = new Product({name,category,price,imgUrl})
+    const productoGuardado = await nuevoProducto.save()
+    res.status(201).json(productoGuardado)
 
 }
 
-export const ObtenerProducto = (req, res) =>{
-    res.json('obteniendo producto')
+export const ObtenerProducto = async (req, res) =>{
+
+    const productos = await Product.find()
+    res.json(productos)
+
 }
 
-export const ObtenerProductoID = (req, res) =>{
+export const ObtenerProductoID = async (req, res) =>{
+
+    const producto = await Product.findById(req.params.productID);
+    res.status(200).json(producto)
     
 }
 
-export const ActualizarProductoID = (req, res) =>{
+export const ActualizarProductoID = async (req, res) =>{
+
+    const ActualizarProducto = await Product.findByIdAndUpdate(req.params.productID, req.body,{
+        new: true
+    })
+    res.status(200).json(ActualizarProducto)
     
 }
 
-export const EliminarProductoID = (req, res) =>{
+export const EliminarProductoID = async (req, res) =>{
+    
+    const {productID} = req.params;
+    await Product.findByIdAndDelete(productID)
+    res.status(204).json()
     
 }
